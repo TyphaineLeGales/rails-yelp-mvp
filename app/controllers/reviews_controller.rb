2a63f25c074dd7427970a756369d2527829ee944
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-15 15:13:28 +0200

    yelp mvp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+  def show
+    @reviews = Review.find(params[:retaurant_id])
+  end
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    allowed_params = params.require(:review).permit(:content, :rating)
+    @review = Review.new(allowed_params)
+    @review.restaurant_id = @restaurant.id
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant_id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    allowed_params = params.require(:review).permit(:content, :rating)
+    @review.update(allowed_params)
+    # redirect_to tasks_path(@task.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # get '/restaurants', to: 'restaurants#index'
-  # get '/restaurants/:id', to: 'restaurants#show'
-  # get '/restaurants/new', to: 'restaurants#new'
-  # post '/restaurants/', to: 'restaurants#create'
-
-  resources :restaurants, only: [:index, :show, :new, :create]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[index new create]
+  end
+  resources :reviews, only: %i[show edit update]
 end
